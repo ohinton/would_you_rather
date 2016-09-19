f4072e772c2ccdbcbe27bbccbeef8197d2d21437
Meaghan Jones and Olivia Hinton <meaghan@>
2016-09-19 16:29:58 -0700

    add total votes function

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
 class Question < ActiveRecord::Base
   validates :choice1, :presence => true
   validates :choice2, :presence => true
+
+  def totalVotes
+    return self.vote1 + self.vote2
+  end
+
+  def average(votes)
+    if self.totalVotes > 0
+      return (votes.to_f/self.totalVotes.to_f * 100).round
+    end
+  end
+
 end
