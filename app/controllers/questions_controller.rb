b381dfcbfcd833e9d0e365745fe056fec011023c
Meaghan Jones and Olivia Hinton <meaghan@>
2016-09-19 10:48:28 -0700

    add vote functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,25 @@
 class QuestionsController < ApplicationController
-  
+
   def index
     @questions = Question.all
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
 
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:vote1, :vote2)
+  end
 
 end
